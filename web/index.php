7319d7d2b6878632b791a52130aeb3c411424d72
Adam Robertson <Adam.Robertson@hostsydney.com>
2013-12-16 14:24:07 +1100

    instructions

--- web/index.php
+++ web/index.php
@@ -8,13 +8,30 @@
 
 <h1>Site created.</h1>
 <p>
-	Setup &amp; push your git repository now using :
+	To setup &amp; push your git repository now using ssh :
+</p>
+
+<p> First ensure your public key is on the server. Then add a record to your ~/.ssh/config : </p>p>
+	<pre class="code">
+Host {server-name}
+	Hostname <?php echo `curl http://169.254.169.254/latest/meta-data/public-ipv4` ?>
+	user {USER}
+	IdentityFile {/full/path/to/your/privatekey.pem
+	</pre>	
+
+<p> Then add a remot and push with : </p>
 
 	<pre class="code">
-		git remote add ec2 ssh://{USER}@<?php echo `curl http://169.254.169.254/latest/meta-data/public-ipv4` ?>{REPO}
-		git push ec2 +master:refs/heads/master
+git remote add ec2 ssh://{server-name}{REPO}
+git push ec2 +master:refs/heads/master
 	</pre>
 </p>
+
+<p> In the future you can then push to the server with just : </p>
+
+<pre>
+git push ec2 {branch}
+</pre>
 	
 </body>
 </html>
